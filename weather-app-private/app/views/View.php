1d937e815b966f353db49bc8c30cb77f2bde1755
Mo Aziz <46410153+MoAziz123@users.noreply.github.com>
2022-02-28 00:55:49 +0000

    Update View.php

--- weather-app-private/app/views/View.php
+++ weather-app-private/app/views/View.php
@@ -32,7 +32,6 @@ abstract class View{
             <head>
             <meta name="author" content="Mo Aziz">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="" content="">
             <link rel="stylesheet" href="$css_path" type="text/css">
             <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
             </head>
